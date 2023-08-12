009bf71a0bf088dc9d596a82798ce8c14d272d63
rosstimo <rosstimo@isu.edu>
2023-08-12 07:26:43 -0600

    fix pa tray, add polkit

--- autostart.lua
+++ autostart.lua
@@ -1,14 +1,22 @@
 
 local spawn   = require("awful.spawn")
 
+spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 spawn.with_shell("/usr/bin/nm-applet")
 spawn.with_shell("/usr/bin/xfce4-power-manager")
 spawn.with_shell("/usr/bin/blueman-applet")
+
 -- prevent multiple pa-applets
-spawn.with_shell("killall pa-applet")
+-- spawn.with_shell("killall pa-applet")
+-- os.execute("sleep 0.25")
+-- spawn.with_shell("/usr/bin/pa-applet")
+
+-- prevent multiple pasystray
+spawn.with_shell("killall pasystray")
 os.execute("sleep 0.25")
-spawn.with_shell("/usr/bin/pa-applet")
-spawn.with_shell("killall kmonad")
-os.execute("sleep 0.25")
-spawn.with_shell("$HOME/.local/bin/kcheck.sh")
+spawn.with_shell("pasystray")
+
+-- spawn.with_shell("killall kmonad")
+-- os.execute("sleep 0.25")
+-- spawn.with_shell("$HOME/.local/bin/kcheck.sh")
 
