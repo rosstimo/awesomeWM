735ae36e67dd75d14c0ef212cedb120f2b2ea2c4
rosstimo <rosstimo@isu.edu>
2023-10-09 14:09:35 -0600

    customize auto start

--- autostart.lua
+++ autostart.lua
@@ -1,11 +1,11 @@
 
 local spawn   = require("awful.spawn")
 
--- spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 spawn.with_shell("/usr/bin/nm-applet")
 spawn.with_shell("/usr/bin/xfce4-power-manager")
 spawn.with_shell("/usr/bin/blueman-applet")
-
+spawn.with_shell("$HOME/.screenlayout/normal.sh")
 -- prevent multiple pa-applets
 -- spawn.with_shell("killall pa-applet")
 -- os.execute("sleep 0.25")
